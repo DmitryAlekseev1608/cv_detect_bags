32d8a558828e2200b9197aefdf6e0984d1f7272c
Dmitry <a89652251224@gmail.com>
2024-01-15 01:22:05 +0300

    new scripts

--- commands.py
+++ commands.py
@@ -1,5 +1,6 @@
 from hydra import compose, initialize
 import fire
+import torch
 
 from cv_detect_bags_experiments.yolo8n import Yolo8n
 from cv_detect_bags_experiments.dataset_coco import DatasetCoco
@@ -14,9 +15,20 @@ def start_yolov8n_alex():
                          cfg_yolo8n.data.path_vid)
     yolo8n_alex.start_work_str_vid()
 
+def start_cv_detect_bags_yolo8n_one_class_coco2017():
+    cv_detect_bags_yolo8n_one_class_coco2017 = Yolo8n(cfg_yolo8n.cv_detect_bags_yolo8n_one_class_coco2017.conf,
+                                                      cfg_yolo8n.cv_detect_bags_yolo8n_one_class_coco2017.classes,
+                                                      cfg_yolo8n.cv_detect_bags_yolo8n_one_class_coco2017.path_model, \
+                                                      cfg_yolo8n.data.path_vid)
+    cv_detect_bags_yolo8n_one_class_coco2017.start_work_str_vid()
+
 def start_load_dataset_coco():
     dataset_coco = DatasetCoco(cfg_dataset_coco.param.segments, cfg_dataset_coco.param.dataset_root_dir)
     dataset_coco.load_dataset_coco()
+
+def start_watch_param_model(model):
+    model = torch.load(f"models/{model}")
+    print(model)
 
 if __name__ == '__main__':
 
